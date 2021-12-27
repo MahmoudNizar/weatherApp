968b4b332c7654d842e9f6a3fce45705ea69a7ac
MahmoudNizar <mhm7182@yahoo.com>
2021-12-27 15:32:40 +0200

    added the IBOutlet.

--- advancedWeatherApp/Controller/ViewController.swift
+++ advancedWeatherApp/Controller/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dailyCollectionView: UICollectionView!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var maxAndMinTemp: UILabel!
+    @IBOutlet weak var tempreature: UILabel!
+    @IBOutlet weak var smallWeatherIcon: UIImageView!
+    @IBOutlet weak var bigWeatherIcon: UIImageView!
+    @IBOutlet weak var dateOfToday: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
